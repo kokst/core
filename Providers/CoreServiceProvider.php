81f9f6fdc03a6b7539be10bdf43e64affb830e32
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-06-19 21:54:23 +0200

    MakeModuleCommand

--- Providers/CoreServiceProvider.php
+++ Providers/CoreServiceProvider.php
@@ -6,4 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class CoreServiceProvider extends ServiceProvider
 {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands([
+            \Kokst\Core\Console\Commands\MakeModuleCommand::class,
+        ]);
+    }
 }
