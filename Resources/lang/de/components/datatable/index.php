3537a1b168870f6ec060a0ea5b1f075cdcc5928b
Nikolas Evers <vintagesucks@users.noreply.github.com>
2019-09-05 11:07:36 +0200

    missing translations for datatable

---
+++ Resources/lang/de/components/datatable/index.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'search' => 'Suche &hellip;',
+    'id' => 'ID',
+    'title' => 'Titel',
+    'activity' => 'Aktivität',
+    'roles' => 'Rollen',
+    'updated' => 'Aktualisiert',
+    'created' => 'Erstellt',
+    'view' => 'Ansehen',
+    'edit' => 'Bearbeiten',
+    'delete' => 'Löschen',
+    'zero' => 'Keine Daten verfügbar',
+];
